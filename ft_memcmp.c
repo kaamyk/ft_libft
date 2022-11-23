b57391cc365209da66e4d15c5630a78b276c8f89
Antoine VINCENT <anvincen@e1r6p16.clusters.42paris.fr>
2022-11-23 19:09:00 +0100

    Add memcmp with correct return value

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: anvincen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 14:39:26 by anvincen          #+#    #+#             */
-/*   Updated: 2022/11/11 16:30:40 by anvincen         ###   ########.fr       */
+/*   Updated: 2022/11/23 14:08:24 by anvincen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,25 +15,18 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char	*tmp;
 	unsigned char	*tmp1;
-	int				ctr;
 
 	tmp = (unsigned char *) s1;
 	tmp1 = (unsigned char *) s2;
-	ctr = 0;
 	if (!n)
 		return (0);
 	while (n)
 	{
 		if (*tmp != *tmp1)
-			ctr += *tmp - *tmp1;
+			return (*tmp - *tmp1);
 		n--;
 		tmp++;
 		tmp1++;
 	}
-	if (ctr > 0)
-		return (5);
-	else if (ctr < 0)
-		return (-1);
-	else
-		return (0);
+	return (0);
 }
